223205183ec7e990b3662818aa606dbbb9e024fc
JP Richardson <jprichardson@gmail.com>
2014-12-28 21:00:46 -0600

    stores/accounts-store: made accounts persistent using localStorage

--- src/renderer/js/stores/accounts-store.js
+++ src/renderer/js/stores/accounts-store.js
@@ -2,14 +2,12 @@ var EventEmitter = require('events').EventEmitter
 var util = require('util')
 var blackCoinInfo = require('coininfo')('BC')
 var CoinKey = require('coinkey')
+var S = require('string')
 var _ = require('lodash')
 var AccountConstants = require('../constants/account-constants')
 var AppDispatcher = require('../dispatcher/app-dispatcher')
 
-//note: trying to stick with idiomatic React.js / flux
-
-//todo: encrypt
-var _accounts = {}
+'use strict'
 
 var CHANGE_EVENT = 'change'
 
@@ -17,13 +15,13 @@ function create(name) {
   var ck = CoinKey.createRandom(blackCoinInfo.versions)
 
   //todo: encrypt
-  _accounts[ck.publicAddress] = {
-    id: ck.publicAddress, //not positive if React requires 'id' in stores, don't think so, but need to doublecheck
+  window.localStorage.setItem('account:' + ck.publicAddress, JSON.stringify({
+    id: 'account:' + ck.publicAddress, //not positive if React requires 'id' in stores, don't think so, but need to doublecheck
     wif: ck.privateWif,
     address: ck.publicAddress,
     name: name,
     amount: 0
-  }
+  }))
 }
 
 var AccountStore = {}
@@ -33,7 +31,14 @@ EventEmitter.call({})
 Object.defineProperty(AccountStore, 'accounts', {
   enumerable: true, configurable: true,
   get: function() {
-    return _accounts
+    var accounts = {}
+    for (var i = 0; i < window.localStorage.length; ++i) {
+      var key = window.localStorage.key(i)
+      if (S(key).startsWith('account:')) {
+        accounts[key] = JSON.parse(window.localStorage.getItem(key))
+      }
+    }
+    return accounts
   }
 })
 
