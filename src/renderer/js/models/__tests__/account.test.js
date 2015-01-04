f9110380caafbc92cc60386bb2e50144139b4569
JP Richardson <jprichardson@gmail.com>
2015-01-03 22:09:29 -0600

    src/renderer/js/models/__tests__/account.test: created

---
+++ src/renderer/js/models/__tests__/account.test.js
@@ -0,0 +1,33 @@
+jest.autoMockOff()
+
+var assert = require.requireActual('assert')
+var CoinKey = require.requireActual('coinkey')
+var ci = require.requireActual('coininfo')
+var accountFixtures = require.requireActual('./account.fixtures')
+var blackCoinInfo = ci('BC')
+
+describe('create()', function() {
+  var _acc1 = accountFixtures.valid[0]
+
+  it('should create a new account', function() {
+    jest.setMock('coinkey', {
+      createRandom: function() {
+        var privKey = new Buffer(_acc1.privateKey, 'hex')
+        return new CoinKey(privKey, blackCoinInfo.versions)
+      }
+    })
+
+    var Account = require('../account')
+    var account = Account.create('Savings')
+    
+    assert.equal(account.wif, _acc1.wif)
+    assert.equal(account.address, _acc1.address)
+    assert.equal(account.id, 'account:' + _acc1.address)
+
+    // todo, change to 'balance'
+    assert.equal(account.amount, 0)
+
+    // clean up
+    jest.dontMock('coinkey')
+  })
+})
