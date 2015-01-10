b9c9c81ec1723b398d56fc5b1dca344ca6fa6c9d
JP Richardson <jprichardson@gmail.com>
2015-01-10 06:00:24 -0600

    gulp: watch / build browserify task (watchify has a bug)

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var fs = require('fs-extra')
 var path = require('path')
 var gulp = require('gulp')
 var gulpAtom = require('gulp-atom')
+var livereload = require('gulp-livereload')
 var pkg = require('./package')
 
 var RELEASE_PATH = './release'
@@ -41,5 +42,53 @@ gulp.task('build-atom-app', function() {
     rebuild: false,
     platforms: PLATFORMS
   })
+})
+
+gulp.task('watch-js', function() {
+  var watch = require('gulp-watch')
+  livereload.listen()
+
+  watch(["src/**/*.js", "!src/renderer/js/_bundle.js"], function() {
+    return gulp.start("build-js")
+  })
+})
+
+gulp.task('build-js', function() {
+  console.log('wtf')
+  var gutil = require('gulp-util')
+  var browserify = require('browserify')
+  var path = require('path')
+  var source = require('vinyl-source-stream')
+  //var watchify = require('watchify')
+  //var _ = require('lodash')
+
+  var file = './src/renderer/js/index.js'
+  //var opts = _.extend({debug: true}, watchify.args)
+  var b = browserify({debug: true})
+  
+  b.transform('reactify')
+
+  b.add(file)
+  //b.require('./config/dev.js', {expose: 'config'})
+  b.on('update', rebundle)
+
+  function rebundle(files) {
+    var start = Date.now()
+    files && files.forEach(function(file) {
+      gutil.log(gutil.colors.magenta(path.basename(file)), 'was changed')
+    })
+
+    return b.bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source('_bundle.js'))
+      .pipe(gulp.dest('./src/renderer/js/'))
+      .pipe(livereload())
+      .on('end', function() {
+        var diff = ((Date.now() - start)/1000).toFixed(2) + ' s'
+        gutil.log('Browserify finished after', gutil.colors.magenta(diff))
+      })
+  }
+
+  return rebundle()
 })
 
