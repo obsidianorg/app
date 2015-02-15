4aa4671679c68a8f6ab025a95dd2fa53604caef2
JP Richardson <jprichardson@gmail.com>
2015-02-15 15:30:58 -0600

    browser/blkqt-raw: prompt error if QT client isn't open

--- src/browser/blkqt-raw.js
+++ src/browser/blkqt-raw.js
@@ -1,5 +1,6 @@
 var bitcoin = require('bitcoin')
 var ipc = require('ipc')
+var dialog = require('dialog')
 
 var RPC_DATA = {
   host: 'localhost',
@@ -15,13 +16,23 @@ var _client
 
 function connect() {
   _client = new bitcoin.Client(RPC_DATA)
+
+  // test connection
+  _client.cmd('getinfo', function() {}, function(err) {
+    if (err && err.code !== -1)
+      // let UI load
+      setTimeout(function() {
+        dialog.showErrorBox('Error: ' + err.code, 'Is Blackcoin-qt open?')
+      },1000)
+  })
+
   return _client
 }
 
 ipc.on('blkqt', function(event, obj) {
   var callback = function(err, res) {
     var msg = obj.msg + '-' + obj.token
-    if (err) 
+    if (err)
       event.sender.send(msg, err.message, null)
     else
       event.sender.send(msg, null, res)
