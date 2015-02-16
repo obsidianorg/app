10dc9a96d23cf8f6b8486e28eadc2c67883508b6
JP Richardson <jprichardson@gmail.com>
2015-02-15 23:07:37 -0600

    r/js/components/header: copy stealth address on click

--- src/renderer/js/components/header.react.js
+++ src/renderer/js/components/header.react.js
@@ -1,10 +1,20 @@
+var clipboard = require('clip' + 'board')
 var React = require('react')
+var stealth = require('../lib/stealth')
 var AccountAdd = require('./account-add.react')
 
+// only onefor now
+var sk = stealth.load()
+
 var Header = React.createClass({
+  handleClick: function(e) {
+    clipboard.writeText(sk.toString())
+    console.log(sk + ' copied')
+  },
+
   render: function() {
     return (
-      <header className="header">
+      <header className="header" onClick={this.handleClick}>
         <div className="text-center">
           Accounts
         </div>
