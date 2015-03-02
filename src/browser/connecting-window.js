8653813d1fe45f3f93a0769d74fe51dd0a85b5d0
JP Richardson <jprichardson@gmail.com>
2015-03-01 20:50:30 -0600

    obsidian: created connecting window to show connecting to blackcoin-qt

---
+++ src/browser/connecting-window.js
@@ -0,0 +1,41 @@
+var path = require('path')
+var url = require('url')
+var BrowserWindow = require('browser-window')
+
+var connectingWindow = null
+
+function initAndShow (isTest, callback) {
+  connectingWindow = new BrowserWindow({
+    width: 400,
+    height: 200,
+    show: false,
+    resizable: false,
+    frame: false,
+    transparent: true
+  })
+
+  var indexUrl = url.format({
+    protocol: 'file',
+    pathname: path.resolve(__dirname, '..', 'renderer', 'connecting.html'),
+    slashes: true,
+    hash: isTest ? 'test' : ''
+  })
+
+  connectingWindow.loadUrl(indexUrl)
+
+  connectingWindow.on('closed', function() {
+    connectingWindow = null
+  })
+
+  connectingWindow.webContents.on('did-finish-load', function() {
+    setTimeout(function() {
+      callback(connectingWindow)
+    }, 250)
+  })
+  connectingWindow.show()
+}
+
+module.exports = {
+  initAndShow: initAndShow
+}
+
