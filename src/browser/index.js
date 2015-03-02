a07a70dbd09e4272c7296cc3b77e4b192ff22394
JP Richardson <jprichardson@gmail.com>
2015-03-01 21:12:36 -0600

    browser/index: check for blackcoin-qt is up and running, run it if not

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,21 +1,89 @@
+var dialog = require('dialog')
+var cp = require('child_process')
+var fs = require('fs')
+var util = require('util')
+var app = require('./app')
+var connectingWindow = require('./connecting-window')
 var mainWindow = require('./main-window')
 var blkqt = require('./blkqt-raw')
 var blockListener = require('./block-listener')
+var settings = require('./settings')
+var exe = require('./settings/exe')
+var qtclient = require('./qtclient')
+var spawn = cp.spawn
 
-var _qtClient = blkqt.connect()
+var cfg = settings.initSync()
 
-mainWindow.initAndShow(function(mainWindow) {
-  blockListener.createListener(function(blockHash) {
-    _qtClient.cmd('getblock', blockHash, function(err, blockData) {
-      if (err) return console.error(err)
-      console.dir(blockData.tx)
-     mainWindow.webContents.send('blockchain:tx:listen', blockData.tx)
+if (!fs.existsSync(cfg.settings.exePath)) {
+  dialog.showError('Error', util.format("Can't find %s-qt. Please locate it.", cfg.settings.test ? 'Bitcoin' : 'BlackCoin'))
+  exe.showFindDialog({test: cfg.settings.test}, function(newPath) {
+    cfg.settings.exePath = newPath
+    cfg.settings.saveSync()
+    start()
+  })
+}
+else {
+  start()
+}
+
+function start() {
+  app.ready(function(app) {
+    connectingWindow.initAndShow(cfg.settings.test, function (connectingWindow) {
+      verifyConnected(function(err) {
+        if (err) return dialog.showErrorBox('Error', "Can't connect to the QT client.")
+        connectingWindow.close()
+
+        initMain()
+      })
     })
-  }).listen(14921)
-})
+  })
+}
+
+function verifyConnected(callback) {
+  var started = false
+
+  var checker = setInterval(function() {
+    check()
+  }, 500)
+
+  var to = setTimeout(function() {
+    clearInterval(check)
+    return callback(new Error('Timeout trying to connect.'))
+  }, 120*1000)
+
+  function check() {
+    qtclient.connect(cfg.rpc, function(err, rpcClient) {
+      if (err && !started) {
+        spawn(cfg.settings.exePath)
+        started = true
+        return
+      } else if (!err) {
+        clearInterval(checker)
+        return callback(null, rpcClient)
+      }
+    })
+  }
+}
+
+function initMain() {
+  //var _qtClient = blkqt.connect()
+
+  mainWindow.initAndShow(function(mainWindow) {
+    /*blockListener.createListener(function(blockHash) {
+      _qtClient.cmd('getblock', blockHash, function(err, blockData) {
+        if (err) return console.error(err)
+        console.dir(blockData.tx)
+       mainWindow.webContents.send('blockchain:tx:listen', blockData.tx)
+      })
+    }).listen(14921)*/
+  })
+}
+
+
+
 
 // hacky messaging solution, switch to fancy html errors
 require('ipc').on('error', function(event, message) {
-  require('dialog').showErrorBox('Error', message)
+  dialog.showErrorBox('Error', message)
 })
 
