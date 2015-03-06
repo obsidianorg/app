25cd67392080adac5f7e380b1491e8758794b2c3
JP Richardson <jprichardson@gmail.com>
2015-03-06 15:24:48 -0600

    renderer/js/index: store initial block count

--- src/renderer/js/index.js
+++ src/renderer/js/index.js
@@ -10,3 +10,14 @@ window.onload = function() {
     document.querySelector('.app')
   ) 
 }
+
+// todo: move
+var blkqt = require('./lib/blkqt')
+var LS_KEY = 'lastBlockCount'
+setTimeout(function() {
+  if (window.localStorage.getItem(LS_KEY)) return
+
+  blkqt.getBlockCount(function(err, bc) {
+    window.localStorage.setItem(LS_KEY, bc)
+  })
+}, 500)
