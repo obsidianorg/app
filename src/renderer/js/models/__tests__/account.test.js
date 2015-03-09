6e05a63867663cfe58a97e60b7db7d08945d0da6
JP Richardson <jprichardson@gmail.com>
2015-03-08 21:48:52 -0500

    renderer/js/models/__tests__/account: skip since Account isn't used anymore

--- src/renderer/js/models/__tests__/account.test.js
+++ src/renderer/js/models/__tests__/account.test.js
@@ -1,26 +1,28 @@
-jest.autoMockOff()
+//jest.autoMockOff()
 
 var assert = require('assert')
 var CoinKey = require('coinkey')
 var ci = require('coininfo')
+var quire = require('quire')
 var accountFixtures = require('./account.fixtures')
 var blackCoinInfo = ci('BLK')
 
-describe('Account', function() {
+describe.skip('Account', function() {
   describe('create()', function() {
     var _acc1 = accountFixtures.valid[0]
 
     it('should create a new account', function() {
-      jest.setMock('coinkey', {
+      var stubs = {'coinkey': {
         createRandom: function() {
-          var privKey = new Buffer(_acc1.privateKey, 'hex')
+          var CoinKey = require('coinkey')
+          var privKey = new Buffer(require('./account.fixtures').valid[0].privateKey, 'hex')
           return new CoinKey(privKey, blackCoinInfo.versions)
         }
-      })
+      }}
 
-      var Account = require('../account')
+      var Account = quire('../account', stubs)
       var account = Account.create('Savings')
-      
+
       assert.equal(account.wif, _acc1.wif)
       assert.equal(account.address, _acc1.address)
       assert.equal(account.id, 'account:' + _acc1.address)
@@ -29,7 +31,7 @@ describe('Account', function() {
       assert.equal(account.amount, 0)
 
       // clean up
-      jest.dontMock('coinkey')
+      //jest.dontMock('coinkey')
     })
   })
 
@@ -39,7 +41,7 @@ describe('Account', function() {
     it('should create from wallet import format (private key)', function() {
       var Account = require('../account')
       var account = Account.createFromWif('Spending', acc0.wif)
-      
+
       assert.equal(account.wif, acc0.wif)
       assert.equal(account.address, acc0.address)
       assert.equal(account.id, 'account:' + acc0.address)
