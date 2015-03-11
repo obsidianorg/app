435663bfefba5908e40200092b34efbc3ac12068
JP Richardson <jprichardson@gmail.com>
2015-03-11 14:33:12 -0500

    renderer/js/lib/ipc: extracted out sendIpc() method from blkqt.js

---
+++ src/renderer/js/lib/ipc.js
@@ -0,0 +1,25 @@
+var atom = require('../atom')
+var ipc = atom.ipc
+
+function sendIpc(data, callback) {
+  data.token = Date.now() + Math.random()
+
+  if (callback) {
+    var recpMsg = data.msg + '-' + data.token
+    ipc.once(recpMsg, function() {
+      var args = [].slice.call(arguments)
+
+      // error
+      if (args[0])
+        callback(new Error(args[0]))
+      else
+        callback(null, args[1])
+    })
+  }
+
+  ipc.send(data.msg, data)
+}
+
+module.exports = {
+  sendIpc: sendIpc
+}
