0487a457fcaf16834c12e9b6bdac89fd1aef1075
JP Richardson <jprichardson@gmail.com>
2015-03-13 11:44:46 -0500

    common/accounting/index: simple accounting for both browser and renderer

---
+++ src/common/accounting/index.js
@@ -0,0 +1,17 @@
+var accounting = require('accounting')
+
+var FORMAT = {
+  symbol: "BLK",
+  format: "%v %s",
+  precision: 4
+}
+
+function formatMoney (money) {
+  return accounting.formatMoney(money, FORMAT)
+}
+
+module.exports = {
+  FORMAT: FORMAT,
+  formatMoney: formatMoney,
+  fm: formatMoney
+}
