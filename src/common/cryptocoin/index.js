43f8e0ea1add56056a3c14636eff3a556f0074a5
JP Richardson <jprichardson@gmail.com>
2015-03-13 11:49:52 -0500

    common/cryptocoin/index: BLK specific cryptocoin

---
+++ src/common/cryptocoin/index.js
@@ -0,0 +1,23 @@
+var coinInfo = require('coininfo')
+var CoinKey = require('coinkey')
+
+function create(coin) {
+  coin = coin || 'BLK'
+  var info = coinInfo(coin)
+
+  var oldCoinKeyCreateRandom = CoinKey.createRandom
+  CoinKey.createRandom = function() {
+    return oldCoinKeyCreateRandom(info.versions)
+  }
+
+  var cc = {
+    CoinKey: CoinKey,
+    info: info
+  }
+
+  return cc
+}
+
+module.exports = {
+  create: create
+}
