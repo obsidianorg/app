b5dbf9348377ef343b52b9646e17171e8aefa25e
JP Richardson <jprichardson@gmail.com>
2015-03-16 02:59:19 -0500

    renderer/js/components/app: bugfix removed PaymentsStore initialization

--- src/renderer/js/components/app.react.js
+++ src/renderer/js/components/app.react.js
@@ -2,6 +2,7 @@ var React = require('react')
 var Sidebar = require('./sidebar.react')
 var Header = require('./header.react')
 var SendForm = require('./send-form.react')
+var PaymentStore = require('../stores/payments-store')
 
 var App = React.createClass({
   getInitialState: function() {
