e9c9f3956ebb85fe9f9774a9abbf8edcbf94d8f8
JP Richardson <jprichardson@gmail.com>
2015-05-28 14:45:24 -0500

    src/db/keydb: use Stealth.fromRandom()

--- src/db/keydb.js
+++ src/db/keydb.js
@@ -1,24 +1,13 @@
-var CoinKey = require('coinkey')
 var Stealth = require('stealth')
 var window = require('../domwindow')
 
-var STEALTH_CONSTANT = 0x27
+var STEALTH_CONSTANT = 39
 var LS_KEY = 'sk'
-
-function create (scan, payload) {
-  return new Stealth({
-    scanPrivKey: scan.privateKey,
-    scanPubKey: scan.publicKey,
-    payloadPrivKey: payload.privateKey,
-    payloadPubKey: payload.publicKey,
-    version: STEALTH_CONSTANT
-  })
-}
 
 function load () {
   var skData = window.localStorage.getItem(LS_KEY)
   if (skData == null) {
-    var sk = create(CoinKey.createRandom(), CoinKey.createRandom())
+    var sk = Stealth.fromRandom({version: STEALTH_CONSTANT})
     window.localStorage.setItem(LS_KEY, sk.toJSON())
     return sk
   } else {
@@ -27,6 +16,5 @@ function load () {
 }
 
 module.exports = {
-  create: create,
   load: load
 }
