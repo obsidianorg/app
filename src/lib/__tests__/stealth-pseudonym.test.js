e3596148e033d5ad7adc41e5149c329495dd44b9
JP Richardson <jprichardson@gmail.com>
2015-05-30 06:56:37 -0500

    src/lib/__tests__/stealth-pseudonym: didn't have describe statement for createRegistryTx

--- src/lib/__tests__/stealth-pseudonym.test.js
+++ src/lib/__tests__/stealth-pseudonym.test.js
@@ -13,37 +13,39 @@ var blkqtStub = {
 var cryptocoin = _.assign({'@noCallThru': true}, require('../../common/cryptocoin'))
 
 describe('stealth-pseudonym', function () {
-  it('should create a pseudonym registery transaction', function (done) {
-    var f1 = fixtures.createTx.valid[0]
-    var blkqt = _.assign({
-      getUnspents: function (address, callback) {
-        callback(null, f1.utxos)
-      },
-      getNewAddress: function (callback) {
-        callback(null, f1.standardOutputs[0].address)
-      },
-      getWif: function (address, callback) {
-        callback(null, f1.utxoKeys[0])
-      }
-    }, blkqtStub)
+  describe('createRegistryTx', function () {
+    it('should create a pseudonym registery transaction', function (done) {
+      var f1 = fixtures.createTx.valid[0]
+      var blkqt = _.assign({
+        getUnspents: function (address, callback) {
+          callback(null, f1.utxos)
+        },
+        getNewAddress: function (callback) {
+          callback(null, f1.standardOutputs[0].address)
+        },
+        getWif: function (address, callback) {
+          callback(null, f1.utxoKeys[0])
+        }
+      }, blkqtStub)
 
-    var stubs = {
-      '../lib/blkqt': blkqt,
-      '../common/cryptocoin': cryptocoin,
-      '../blockchain/txutils': {
-        setCurrentTime: function (tx) {
-          tx.timestamp = f1.timestamp
+      var stubs = {
+        '../lib/blkqt': blkqt,
+        '../common/cryptocoin': cryptocoin,
+        '../blockchain/txutils': {
+          setCurrentTime: function (tx) {
+            tx.timestamp = f1.timestamp
+          }
         }
       }
-    }
 
-    var stealthPseudonym = proxyquire('../stealth-pseudonym', stubs)
-    var stealthKey = Stealth.fromJSON(JSON.stringify(f1.stealth))
-    stealthPseudonym.createRegistryTx(f1.pseudonym, stealthKey, function (err, tx) {
-      assert.ifError(err)
-      var hex = txUtils.serializeToHex(tx)
-      assert.strictEqual(f1.txHex, hex)
-      done()
+      var stealthPseudonym = proxyquire('../stealth-pseudonym', stubs)
+      var stealthKey = Stealth.fromJSON(JSON.stringify(f1.stealth))
+      stealthPseudonym.createRegistryTx(f1.pseudonym, stealthKey, function (err, tx) {
+        assert.ifError(err)
+        var hex = txUtils.serializeToHex(tx)
+        assert.strictEqual(f1.txHex, hex)
+        done()
+      })
     })
   })
 })
