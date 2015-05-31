b512929358b4060ddb75c38ca9483865e0051761
JP Richardson <jprichardson@gmail.com>
2015-05-31 17:26:51 -0500

    src/lib/__tests__/stealth-pseudonym: clean up with babel / stubo

--- src/lib/__tests__/stealth-pseudonym.test.js
+++ src/lib/__tests__/stealth-pseudonym.test.js
@@ -1,42 +1,23 @@
 var assert = require('assert')
 var proxyquire = require('proxyquire')
-var _ = require('lodash')
+var stubo = require('stubo')
 var Stealth = require('stealth')
 var txUtils = require('../../blockchain/txutils')
 var fixtures = require('./stealth-pseudonym.fixtures')
 
 /* global describe, it */
 
-var blkqtStub = {
-  '@noCallThru': true // for proxyquire
-}
-var cryptocoin = _.assign({'@noCallThru': true}, require('../../common/cryptocoin'))
+// var cryptocoin = _.assign({'@noCallThru': true}, require('../../common/cryptocoin'))
 
 describe('stealth-pseudonym', function () {
   describe('createRegistryTx', function () {
     it('should create a pseudonym registery transaction', function (done) {
       var f0 = fixtures.createTx.valid[0]
-      var blkqt = _.assign({
-        getUnspents: function (address, callback) {
-          callback(null, f0.utxos)
-        },
-        getNewAddress: function (callback) {
-          callback(null, f0.standardOutputs[0].address)
-        },
-        getWif: function (address, callback) {
-          callback(null, f0.utxoKeys[0])
-        }
-      }, blkqtStub)
-
-      var stubs = {
-        '../lib/blkqt': blkqt,
-        '../common/cryptocoin': cryptocoin,
-        '../blockchain/txutils': {
-          setCurrentTime: function (tx) {
-            tx.timestamp = f0.timestamp
-          }
-        }
-      }
+      var stubs = {'../lib/blkqt': {'@noCallThru': true}}
+      stubo(stubs, '../lib/blkqt', 'getUnspents', (address, callback) => callback(null, f0.utxos))
+      stubo(stubs, '../lib/blkqt', 'getNewAddress', (callback) => callback(null, f0.standardOutputs[0].address))
+      stubo(stubs, '../lib/blkqt', 'getWif', (address, callback) => callback(null, f0.utxoKeys[0]))
+      stubo(stubs, '../blockchain/txutils', 'setCurrentTime', (tx) => tx.timestamp = f0.timestamp)
 
       var stealthPseudonym = proxyquire('../stealth-pseudonym', stubs)
       var stealthKey = Stealth.fromJSON(JSON.stringify(f0.stealth))
