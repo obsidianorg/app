12fc03d257b2155f2bcd21174be198a97001552b
JP Richardson <jprichardson@gmail.com>
2015-07-20 14:46:52 -0500

    src/index: hook console messages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 require('./babel/hook')
-
 require('./devtools')
+
+var remote = require('remote')
+var remoteConsole = remote.require('console')
+
+console.log = function () {
+  remoteConsole.log.apply(remoteConsole, arguments)
+}
+
+console.dir = function () {
+  remoteConsole.dir.apply(remoteConsole, arguments)
+}
 
 // annoying popup an error box instead of silent console output
 window.onerror = function (message, url, lineno, colno, error) {
