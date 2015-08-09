640b066e383ae55f2131ec5974f77b8e5fb1818c
JP Richardson <jprichardson@gmail.com>
2015-08-09 12:29:41 -0500

    obsidian: removed unnecessary div.app

--- src/ui/index.react.js
+++ src/ui/index.react.js
@@ -2,10 +2,7 @@ var React = require('react')
 var App = require('./app.react.js')
 
 function renderApp () {
-  React.render(
-    <App/>,
-    document.querySelector('.app')
-  )
+  React.render(<App/>, document.body)
 }
 
 module.exports = {
