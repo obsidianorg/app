7d2a8948e7a3d01714d7a7784e518f8e7a37ef86
JP Richardson <jprichardson@gmail.com>
2015-08-17 15:59:01 -0500

    obisidan: upgraded redux from 1.0.0-rc to 1.0.1

--- src/ui/app.react.js
+++ src/ui/app.react.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { TabbedArea, TabPane } from 'react-bootstrap'
-import { provide } from 'react-redux'
+import { Provider } from 'react-redux'
 import { store } from '#flux'
 import Sidebar from './sidebar'
 import Header from './header.react'
@@ -8,21 +8,26 @@ import Footer from './footer/index.react'
 import SendForm from './send-form'
 import StealthKeyView from './stealth-keys/index.react'
 
-@provide(store)
 export default class App extends React.Component {
   render () {
     return (
-      <div id='container' >
-        <Sidebar />
-        <section id='main-content'>
-          <Header />
-          <TabbedArea defaultActiveKey={1}>
-            <TabPane eventKey={1} tab='Send'><SendForm /></TabPane>
-            <TabPane eventKey={2} tab='Stealth'><StealthKeyView /></TabPane>
-          </TabbedArea>
-        </section>
-        <Footer />
-      </div>
+      <Provider store={ store }>
+        { () => {
+          return (
+            <div id='container' >
+              <Sidebar />
+              <section id='main-content'>
+                <Header />
+                <TabbedArea defaultActiveKey={1}>
+                  <TabPane eventKey={1} tab='Send'><SendForm /></TabPane>
+                  <TabPane eventKey={2} tab='Stealth'><StealthKeyView /></TabPane>
+                </TabbedArea>
+              </section>
+              <Footer />
+            </div>
+          )
+        }}
+      </Provider>
     )
   }
 }
