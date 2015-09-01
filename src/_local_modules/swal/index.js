6aefc530c7b3339f515b75638d3acd50bfce60df
JP Richardson <jprichardson@gmail.com>
2015-09-01 03:24:43 -0500

    #swal: added error, prompt, success

--- src/_local_modules/swal/index.js
+++ src/_local_modules/swal/index.js
@@ -15,6 +15,49 @@ swal.confirm = function confirm (args, callback) {
     customClass: 'sweet-alert-obsidian',
     ...args
   }, function () {
-    callback.apply(this, arguments)
+    return callback.apply(this, arguments)
   })
+}
+
+swal.prompt = function prompt (args, callback) {
+  _swal({
+    type: 'input',
+    confirmButtonColor: '#DD6B55',
+    showCancelButton: true,
+    customClass: 'sweet-alert-obsidian',
+    closeOnConfirm: false,
+    // animation: 'slide-from-top',
+    /* inputPlaceholder: 'Write something' */
+    ...args
+  }, function (cancelOrInputVal) {
+    if (cancelOrInputVal === false) return true // cancel button pressed, close it
+    if (typeof args.validate === 'function') {
+      let message = args.validate(cancelOrInputVal)
+      if (message) {
+        _swal.showInputError(message)
+        return false
+      }
+    }
+    return callback.apply(this, arguments)
+  })
+}
+
+swal.error = function error (args, callback) {
+  _swal({
+    type: 'error',
+    title: 'Error',
+    customClass: 'sweet-alert-obsidian',
+    showCancelButton: false,
+    ...args
+  }, callback)
+}
+
+swal.success = function success (args, callback) {
+  _swal({
+    type: 'success',
+    title: 'Success',
+    customClass: 'sweet-alert-obsidian',
+    showCancelButton: false,
+    ...args
+  }, callback)
 }
